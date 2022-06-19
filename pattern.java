4ab9760f7c8bf4160fcf2af4e0c83ee3d45da890
ReetuChauhann <chauhanreetu00@gmail.com>
2022-06-19 17:12:00 +0530

    pattern printing

---
+++ pattern.java
@@ -0,0 +1,12 @@
+public class pattern {
+    public static void main(String[] args) {
+        int letter=65;
+        for(int i=0; i<5; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print((char)(letter+i));
+            }
+            System.out.println();
+        }
+    }
+    
+}
